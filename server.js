bf5a953f8995c66ef9afc6c6fa99e1e14c5843ae
Volodymyr Baisa <biosvova@gmail.com>
2020-11-09 18:16:17 -0500

    configure styles and packages, add project structure, configure express server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const exphbs = require("express-handlebars");
+
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+    partialsDir: path.join(__dirname, "/views/partials/"),
+  })
+);
+
+app.set("view engine", "handlebars");
+
+app.listen(PORT, () => {
+  console.log("🌎 => live on http://localhost:%s", PORT);
+});
