0c076bd19d5ca6674a8118ea6ae519fdcadceef1
Volodymyr Baisa <biosvova@gmail.com>
2020-11-16 20:38:41 -0500

    Revert "update"
    This reverts commit e99b3f26663d99f1971f2e86c24ba6021529656d.

---
+++ server.js
@@ -0,0 +1,37 @@
+const config = require("./config");
+const express = require("express");
+const path = require("path");
+const db = require("./models");
+const apiRoutes = require("./routes/api-routes");
+const htmlRoutes = require("./routes/html-routes");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const exphbs = require("express-handlebars");
+const helpers = require("./views/helpers");
+
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+    partialsDir: path.join(__dirname, "/views/partials/"),
+    helpers: helpers,
+  })
+);
+
+app.set("view engine", "handlebars");
+
+app.use("/api", apiRoutes);
+app.use(htmlRoutes);
+
+db.sequelize.sync(/*{ force: config.sync }*/).then(async () => {
+  app.listen(PORT, () => {
+    console.log(`🌎 => live on http://localhost:${PORT}`);
+  });
+});
